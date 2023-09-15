27089226717cdc1272be9b6d4f89c4bb3893ef5d
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-15 15:50:50 -0500

    Added BookRepository extending JPARepository to enable CRUD funtionality

--- src/main/java/com/example/LibraryBookManager/repository/BookRepository.java
+++ src/main/java/com/example/LibraryBookManager/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.LibraryBookManager.repository;
 
-public interface BookRepository {
+import com.example.LibraryBookManager.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
