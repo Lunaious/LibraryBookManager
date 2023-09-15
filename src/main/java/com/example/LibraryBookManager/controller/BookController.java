9c72bded9cff8fdeaffcfa244e17af7f9232b99a
Lunaious <52602883+Lunaious@users.noreply.github.com>
2023-09-15 16:23:48 -0500

    Implemented BookController and BookService book management logic

--- src/main/java/com/example/LibraryBookManager/controller/BookController.java
+++ src/main/java/com/example/LibraryBookManager/controller/BookController.java
@@ -1,4 +1,49 @@
 package com.example.LibraryBookManager.controller;
 
+import com.example.LibraryBookManager.model.Book;
+import com.example.LibraryBookManager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/")
+    public String getBookList(Model model){
+        List<Book> books = bookService.getBookList();
+        model.addAttribute("books", books);
+        return "book-list";
+    }
+
+    @GetMapping("/getbookform")
+    public String getBookForm(Model model){
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "book-form";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model){
+        Book book = bookService.edit(id);
+        model.addAttribute("book", book);
+        return "book-form";
+    }
+
+    @PostMapping("/submitform")
+    public String submitForm(@ModelAttribute Book book){
+        bookService.submitForm(book);
+        return "redirect:/books/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        bookService.delete(id);
+        return "redirect:/books/";
+    }
 }
--- src/main/java/com/example/LibraryBookManager/service/BookService.java
+++ src/main/java/com/example/LibraryBookManager/service/BookService.java
@@ -1,4 +1,28 @@
 package com.example.LibraryBookManager.service;
 
+import com.example.LibraryBookManager.model.Book;
+import com.example.LibraryBookManager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    public List<Book> getBookList() {
+        return bookRepository.findAll();
+    }
+
+    public Book edit(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void submitForm(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void delete(Long id) {
+        bookRepository.deleteById(id);
+    }
 }
